d9694c9f5a620d393070ae1c432ab01740ccad78
C. Scott Ananian <cscott@cscott.net>
2011-05-14 20:22:38 -0400

    Improve str_escape function on chrome/webkit.

--- crender.js
+++ crender.js
@@ -710,8 +710,39 @@ var make_crender = function() {
             // abuse toSource() to properly quote a string value.
             return s.toSource().slice(12,-2);
         }
-        // FIXME value isn't escaped on chrome/webkit
-        return '"' + s.toString() + '"';
+        // Erg, use hand-coded version
+        var quotes = '"';
+        if (s.indexOf('"') !== -1 && s.indexOf("'") === -1) {
+            quotes = "'";
+        }
+
+        var table = {};
+        table["\n"] = "n";
+        table["\r"] = "r";
+        table["\f"] = "f";
+        table["\b"] = "b";
+        table["\t"] = "t";
+        table["\\"] = "\\";
+        table[quotes] = quotes;
+
+        var result = "", i=0;
+        while (i < s.length) {
+            var c = s.charAt(i);
+            if (table[c]) {
+                result += "\\" + table[c];
+            } else if (c < ' ' || c > '~') {
+                // XXX allow some accented UTF-8 characters (printable ones)?
+                var cc = c.charCodeAt(0).toString(16);
+                while (cc.length < 4) {
+                    cc = "0" + cc;
+                }
+                result += "\\u" + cc;
+            } else {
+                result += c;
+            }
+            i += 1;
+        }
+        return quotes + result + quotes;
     };
 
     var dispatch = {};
