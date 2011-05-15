b38a4a7de976b4c178413afb00c9b42bc369fae3
C. Scott Ananian <cscott@cscott.net>
2011-05-14 22:04:00 -0400

    Whitespace fixes to binterp.js.

--- binterp.js
+++ binterp.js
@@ -534,24 +534,24 @@ var make_binterp = function(bytecode_table) {
             // http://blog.stevenlevithan.com/archives/faster-trim-javascript
             var str = this;
             if (str.length === 0) { return str; }
-	    var whitespace = ' \n\r\t\f\u000b\u00a0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
+            var whitespace = ' \n\r\t\f\u000b\u00a0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
             var i = 0;
             while (i < str.length) {
-		if (whitespace.indexOf(str.charAt(i)) === -1) {
-		    str = str.substring(i);
-		    break;
-		}
+                if (whitespace.indexOf(str.charAt(i)) === -1) {
+                    str = str.substring(i);
+                    break;
+                }
                 i += 1;
-	    }
+            }
             i = str.length - 1;
             while ( i >= 0 ) {
-		if (whitespace.indexOf(str.charAt(i)) === -1) {
-		    str = str.substring(0, i + 1);
-		    break;
-		}
+                if (whitespace.indexOf(str.charAt(i)) === -1) {
+                    str = str.substring(0, i + 1);
+                    break;
+                }
                 i -= 1;
             }
-	    return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
+            return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
         };
         Array.prototype.push = function() {
             var i = 0, j = (1*this.length) || 0;
