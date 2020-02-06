f654ce8181287e1740a32c94976c90818c1b53a8
C. Scott Ananian <cscott@cscott.net>
2020-02-06 00:42:39 -0500

    Define String#charCodeAt in stdlib

--- stdlib.js
+++ stdlib.js
@@ -4,6 +4,36 @@
 // Used for bytecode interpreters (both TurtleScript and native).
 define(['text!stdlib.js'], function make_stdlib(stdlib_source) {
     var init = /*CUT HERE*/function() {
+        String.prototype.codePointAt = function(position) {
+            if (this === null || this === undefined) {
+                Object.Throw('TypeError'); /*XXX*/
+            }
+            var string = String(this);
+            var size = string.length;
+            // `ToInteger`
+            var index = position ? Number(position) : 0;
+            if (isNaN(index)) { // better `isNaN`
+                index = 0;
+            }
+            // Account for out-of-bounds indices:
+            if (index < 0 || index >= size) {
+                return undefined;
+            }
+            // Get the first code unit
+            var first = string.charCodeAt(index);
+            var second;
+            if ( // check if it’s the start of a surrogate pair
+                first >= 0xD800 && first <= 0xDBFF && // high surrogate
+                    size > index + 1 // there is a next code unit
+            ) {
+                second = string.charCodeAt(index + 1);
+                if (second >= 0xDC00 && second <= 0xDFFF) { // low surrogate
+                    // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+                    return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+                }
+            }
+            return first;
+        };
         String.prototype.indexOf = function(searchValue, from) {
             var i = from || 0;
             var j = 0;
