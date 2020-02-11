fac33064bf2db3210a66baaf7c0268522c59c987
C. Scott Ananian <cscott@cscott.net>
2020-02-11 14:45:46 -0500

    Tweak the top level definitions (add a new more things)

--- top-level.js
+++ top-level.js
@@ -1,8 +1,8 @@
 // Top-level definitions suitable for parsing TurtleScript modules.
 define([], function() {
     var TOP_LEVEL = "isFinite parseInt parseFloat isNaN "+
-        "Array Boolean String Function Math Number Object "+
-        "JSON RegExp SyntaxError "+
+        "Array Boolean Error Function Math Number Object "+
+        "JSON RangeError RegExp String SyntaxError TypeError "+
         "console arguments now define document eval globalThis";
     return TOP_LEVEL;
 });
